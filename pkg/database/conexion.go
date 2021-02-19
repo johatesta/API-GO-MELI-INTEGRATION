3c425f30937244951bc6643518cf23b22ac819b9
johatesta <66925207+johatesta@users.noreply.github.com>
2021-02-19 17:07:17 -0300

    Create conexion.go

---
+++ pkg/database/conexion.go
@@ -0,0 +1,15 @@
+package database
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() gorm.DB{
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(127.0.0.1:3306)/meli_app?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return *db
+}
